2e25c185144ca1c26fd538e6fcccbf0f43476839
yanshiuh <yanshiuh12372@gmail.com>
2023-04-08 16:25:03 +0800

    project setup

---
+++ src/api.js
@@ -0,0 +1,5 @@
+const API_URL = "http://localhost:8080/api";
+
+const userApi = API_URL + "/user";
+
+export default userApi;
---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import userApi from "../api";
+
+function Home() {
+  const [data, setData] = useState([]);
+
+  const fetchAllUsers = async () => {
+    const fetchUsers = await axios
+      .get(userApi)
+      .then((res) => {
+        const data = res.data;
+        setData(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {data.map((each) => (
+        <p key={each.id}>
+          {each.id} {each.name} {each.country}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
